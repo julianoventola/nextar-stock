dc0fd8ba5173c737ef0d6b93c8f7ec85cddaa3ca
Juliano Ventola <ventolajuliano@gmail.com>
2026-04-14 17:00:23 -0300

    fix: json-server to vercel

--- api/server.ts
+++ api/server.ts
@@ -1,10 +1,16 @@
-import { create, router, defaults, rewriter } from "json-server";
-const server = create();
-const database = router('db.json');
-const middlewares = defaults();
+import jsonServer from 'json-server';
+import path from 'path';
+
+const server = jsonServer.create();
+// IMPORTANT: Use the path to your db.json
+const router = jsonServer.router(path.join(__dirname, '../db.json'));
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(rewriter({ '/api/*': '/$1' })); // Redirects /api/posts to /posts
-server.use(database);
+// Optional: Rewrite routes if your frontend expects /api/something
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1'
+}));
+server.use(router);
 
-export default server
+export default server;
